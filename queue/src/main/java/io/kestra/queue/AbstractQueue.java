Cloud-lian/Kestra_1.2.3

feat(system): use virtual threads to asynchronously emit queue messages

queue/src/main/java/io/kestra/queue/AbstractQueue.java
@@ -29,7 +29,8 @@ abstract class AbstractQueue<T extends Event> implements GenericQueueInterface<T
         this.cls = cls;
         this.queueService = queueService;
         int maxAsyncThreads = Math.max(4, executorsUtils.getAllocatedCpuCores());
-        this.asyncPoolExecutor = executorsUtils.maxCachedThreadPool(maxAsyncThreads, "queue-async-" + queueName());
+        // this executor service is used to asynchronously emit messages to the queue, as emitting is I/O bound, we can use virtual threads here
+        this.emitAsyncPoolExecutor = executorsUtils.maxCachedVirtualThreadPool(maxAsyncThreads, "queue-async-" + queueName());
         this.emitCounter = metricRegistry.counter(MetricRegistry.METRIC_QUEUE_EMIT_COUNT, MetricRegistry.METRIC_QUEUE_EMIT_COUNT_DESCRIPTION, MetricRegistry.TAG_QUEUE_NAME, queueName());
 
         if (LOG.isDebugEnabled()) {
@@ -85,6 +86,6 @@ abstract class AbstractQueue<T extends Event> implements GenericQueueInterface<T
 
     @Override
     public void close() {
-        this.asyncPoolExecutor.shutdown();
+        this.emitAsyncPoolExecutor.shutdown();
     }
 }

flcarre/kestra

queue/src/main/java/io/kestra/queue/AbstractQueue.java
@@ -34,7 +34,7 @@ abstract class AbstractQueue<T extends Event> implements GenericQueueInterface<T
         this.cls = cls;
         this.queueService = queueService;
         int maxAsyncThreads = Math.max(4, executorsUtils.getAllocatedCpuCores());
-        this.asyncPoolExecutor = executorsUtils.maxCachedThreadPool(maxAsyncThreads, "queue-async-" + queueName());
+        this.asyncPoolExecutor = executorsUtils.maxCachedVirtualThreadPool(maxAsyncThreads, "queue-async-" + queueName());
         this.emitCounter = metricRegistry.counter(MetricRegistry.METRIC_QUEUE_EMIT_COUNT, MetricRegistry.METRIC_QUEUE_EMIT_COUNT_DESCRIPTION, MetricRegistry.TAG_QUEUE_NAME, queueName());
 
         if (LOG.isDebugEnabled()) {